77d00f3de04abc1b1892104bb14bcc45f18f7d6c
Hui Wang <huiw3939@gmail.com>
2017-07-20 09:38:34 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Dog
+
+@@all = []
+
+attr_accessor :name
+
+def initialize(name)
+@name = name
+  @@all << self
+end
+
+
+
+def self.clear_all
+  @@all = []
+end
+
+def self.all
+  @@all.each do |x|
+    puts x.name
+  end
+end
+
+
+end
